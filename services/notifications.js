486a024d2a0ac85231a948854f8bf3ff30d05660
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-17 19:55:08 -0400

    Fixed Notification spelling

--- services/notifications.js
+++ services/notifications.js
@@ -1,9 +1,9 @@
 const {db} = require('./dbConnect');
-const NotficationService = {};
+const NotificationService = {};
 
-NotficationService.create = (user_action_id, notification_type, follower_action_id, like_action_id, comment_action_id) => {
+NotificationService.create = (user_action_id, notification_type, follower_action_id, like_action_id, comment_action_id) => {
     const sql = `
-    INSERT INTO notfications (user_action_id, notification_type, follower_action_id, like_action_id, comment_action_id) VALUES
+    INSERT INTO notifications (user_action_id, notification_type, follower_action_id, like_action_id, comment_action_id) VALUES
     ($[user_action_id], $[notification_type], $[follower_action_id], $[like_action_id], $[comment_action_id]);`;
 
     return db.one(sql, { user_action_id, notification_type, follower_action_id, like_action_id, comment_action_id });
