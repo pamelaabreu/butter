4df3cdb8aa8a8dbe3cc9e8cdb65237f50171cf0c
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-17 19:54:42 -0400

    Created Notifications.create and read functions

--- services/notifications.js
+++ services/notifications.js
@@ -1,4 +1,24 @@
 const {db} = require('./dbConnect');
 const NotficationService = {};
 
+NotficationService.create = (user_action_id, notification_type, follower_action_id, like_action_id, comment_action_id) => {
+    const sql = `
+    INSERT INTO notfications (user_action_id, notification_type, follower_action_id, like_action_id, comment_action_id) VALUES
+    ($[user_action_id], $[notification_type], $[follower_action_id], $[like_action_id], $[comment_action_id]);`;
+
+    return db.one(sql, { user_action_id, notification_type, follower_action_id, like_action_id, comment_action_id });
+};
+
+NotficationService.read = (id) => {
+    const sql = `
+    SELECT
+        notifications.*
+    FROM notifications
+    WHERE
+        notifications.id = $[id]
+    `;
+
+    return db.one(sql, { id });
+};
+
 module.exports = NotficationService;
