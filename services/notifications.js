359beb84f21c8f4df637ddb914d130f3261cd19f
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-18 16:59:57 -0400

    created NotificationService.readAllNotfications and updated functions to have user_received_action_id

--- services/notifications.js
+++ services/notifications.js
@@ -1,12 +1,12 @@
 const {db} = require('./dbConnect');
 const NotificationService = {};
 
-NotificationService.create = (user_action_id, notification_type, follower_action_id, like_action_id, comment_action_id) => {
+NotificationService.create = (user_action_id, user_received_action_id, notification_type, follower_action_id, like_action_id, comment_action_id) => {
     const sql = `
-    INSERT INTO notifications (user_action_id, notification_type, follower_action_id, like_action_id, comment_action_id) VALUES
-    ($[user_action_id], $[notification_type], $[follower_action_id], $[like_action_id], $[comment_action_id]);`;
+    INSERT INTO notifications (user_action_id, user_received_action_id, notification_type, follower_action_id, like_action_id, comment_action_id) VALUES
+    ($[user_action_id], $[user_received_action_id], $[notification_type], $[follower_action_id], $[like_action_id], $[comment_action_id]);`;
 
-    return db.one(sql, { user_action_id, notification_type, follower_action_id, like_action_id, comment_action_id });
+    return db.one(sql, { user_action_id, user_received_action_id, notification_type, follower_action_id, like_action_id, comment_action_id });
 };
 
 NotficationService.read = (id) => {
@@ -21,7 +21,7 @@ NotficationService.read = (id) => {
     return db.one(sql, { id });
 };
 
-NotficationService.update = (user_action_id, notification_type, follower_action_id, like_action_id, comment_action_id) => {
+NotficationService.update = (user_action_id, user_received_action_id, notification_type, follower_action_id, like_action_id, comment_action_id) => {
     const updated_at = Date.now();
     const sql = `
     UPDATE notifications
@@ -31,12 +31,13 @@ NotficationService.update = (user_action_id, notification_type, follower_action_
         notification_type = $[notification_type],
         follower_action_id = $[follower_action_id],
         like_action_id = $[like_action_id],
-        comment_action_id = $[comment_action_id]
+        comment_action_id = $[comment_action_id],
+        user_received_action_id = $[user_received_action_id]
     WHERE
         id=$[id]
     `;
 
-    return db.none(sql, { id, updated_at, user_action_id, notification_type, follower_action_id, like_action_id, comment_action_id });
+    return db.none(sql, { id, updated_at, user_action_id, user_received_action_id, notification_type, follower_action_id, like_action_id, comment_action_id });
 };
 
 NotficationService.delete = (id) => {
@@ -45,6 +46,17 @@ NotficationService.delete = (id) => {
     `;
 
     return db.none(sql, { id });
+};
+
+NotficationService.readAllNotfications = (id) => {
+    const sql = `
+    SELECT 
+        notifications.*
+    FROM notifications
+    WHERE
+        notifications.user_received_action_id = $[id]
+    `;
+    return db.any(sql, { id });
 };
 
 module.exports = NotficationService;
