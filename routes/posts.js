aa9e695dd19d329b270307b976dbe09bf39743ba
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-19 15:25:56 -0400

    created post read function

--- routes/posts.js
+++ routes/posts.js
@@ -6,10 +6,22 @@ const PostService = require('../services/posts');
 postRouter.post('/', (req, res, next) => {
     const { user_posted_id, tag_id, content_url, title, summary, caption } = req.body;
   
-    console.log(req.body)
     PostService.create(user_posted_id, tag_id, content_url, title, summary, caption)
       .then(() => {
         res.json({success: `Created Post with title ${title}.`});
+      })
+      .catch(err => {
+        next(err);
+      })
+  });
+
+// GET - READ 
+postRouter.get('/:id/', (req, res, next) => {
+    const {id} = req.params;
+  
+    PostService.read(id)
+      .then(data => {
+        res.json(data);
       })
       .catch(err => {
         next(err);
