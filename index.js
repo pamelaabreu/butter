14e6855c6cf4bc5e9729daf0ba0862826d9b8ee2
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-19 14:52:36 -0400

    Added postRouter

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const postRouter = require('./routes/posts');
+
 // MIDDLEWARE NEEDED
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+
+//ROUTES
+app.use('/post', postRouter);
+
 
 
 app.listen(port, () => {
