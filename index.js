801a13f1633e010a945cb6f68417439768d566e6
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-19 14:49:51 -0400

    Imported bodyParser middleware code

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// MIDDLEWARE NEEDED
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
 
 app.listen(port, () => {
     console.log('Butter API is running on Port: '+port);
