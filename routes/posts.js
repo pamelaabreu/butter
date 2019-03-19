f8d8438f7e675bf5c24953ce1ecc149b3caba1ab
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-19 15:06:00 -0400

    removed the likes and comments parameters in creating a post

--- routes/posts.js
+++ routes/posts.js
@@ -2,4 +2,17 @@ const express = require('express');
 const postRouter = express.Router();
 const PostService = require('../services/posts');
 
+// POST - CREATE 
+postRouter.post('/', (req, res, next) => {
+    const {user_posted_id, tag_id, content_url, title, summary, caption, likes, comments} = req.body;
+  
+    PostService.create(user_posted_id, tag_id, content_url, title, summary, caption, likes, comments)
+      .then(() => {
+        res.json({success: `Created Post with title ${title}.`});
+      })
+      .catch(err => {
+        next(err);
+      })
+  });
+
 module.exports = postRouter;
--- services/posts.js
+++ services/posts.js
@@ -1,12 +1,12 @@
 const {db} = require('./dbConnect');
 const PostService = {};
 
-PostService.create = (user_posted_id, tag_id, content_url, title, summary, caption, likes, comments) => {
+PostService.create = (user_posted_id, tag_id, content_url, title, summary, caption) => {
     const sql = `
-    INSERT INTO posts (user_posted_id, tag_id, content_url, title, summary, caption, likes, comments) VALUES
-    ($[user_posted_id], $[tag_id], $[content_url], $[title], $[summary], $[caption], $[likes], $[comments]);`;
+    INSERT INTO posts (user_posted_id, tag_id, content_url, title, summary, caption) VALUES
+    ($[user_posted_id], $[tag_id], $[content_url], $[title], $[summary], $[caption]);`;
 
-    return db.one(sql, { user_posted_id, tag_id, content_url, title, summary, caption, likes, comments })
+    return db.one(sql, { user_posted_id, tag_id, content_url, title, summary, caption})
 };
 
 PostService.read = (id) => {
