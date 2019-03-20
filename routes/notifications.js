d02b0046741abb814639058ddb9eeb7cc6941815
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-20 01:32:25 -0400

    Created inital setup of notifications route file

---
+++ routes/notifications.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const notificationRouter = express.Router();
+const NotificationService = require('../services/notifications');
+
+module.exports = notificationRouter;
