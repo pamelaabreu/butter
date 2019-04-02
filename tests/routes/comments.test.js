5d39de6df50ad0e7af4e54fb3ebcd9ca6f3174b5
Pamela Abreu <pamelaabreu@pursuit.com>
2019-04-02 17:12:03 -0400

    Modified user test for readAllUsers route

--- tests/routes/comments.test.js
+++ tests/routes/comments.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 jest.mock('../../utils/comments');
-const {createCommentAndNotify,} = require('../../utils/comments')
+const {createCommentAndNotify,} = require('../../utils/comments');
 const { app } = require('../../app');
 
 test("When creating a new comment, expect 200 response", done => {
@@ -12,7 +12,7 @@ test("When creating a new comment, expect 200 response", done => {
             expect(response.status).toBe(200)
             done()
         })
-})
+});
 
 test("When creating a new comment incorrectly, expect 400 response", done => {
     createCommentAndNotify.mockImplementation(() => Promise.reject());
@@ -23,4 +23,4 @@ test("When creating a new comment incorrectly, expect 400 response", done => {
             expect(response.status).toBe(400)
             done()
         })
-})
+});
--- tests/routes/users.test.js
+++ tests/routes/users.test.js
@@ -3,3 +3,15 @@ jest.mock('../../services/users');
 const UserService = require('../../services/users');
 const { app } = require('../../app');
 
+test("When making a get request to /user/all, if DB is successfull, expect 200", done => {
+    UserService.readAllUsers.mockImplementation(() => {
+        return Promise.resolve();
+    });
+    
+    request(app)
+        .get('/user/all')
+        .then(response => {
+            expect(response.status).toBe(200)
+            done()
+        })
+});
